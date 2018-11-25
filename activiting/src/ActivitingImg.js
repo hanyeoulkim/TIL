afb76a2d506ad10ca72d378e8e644509ecc5f74a
hanyeoulkim <rnltthr06@gmail.com>
2018-11-25 21:27:18 +0900

    after carousel install

---
+++ activiting/src/ActivitingImg.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+import ReactDOM from 'react-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import "./ActivitingImg.css";
+
+export default function ActivitingImg() {
+  return <div />;
+}
