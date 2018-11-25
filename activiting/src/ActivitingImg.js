97d05b6fd41245cf0cd7e306e579fb312e939e08
hanyeoulkim <rnltthr06@gmail.com>
2018-11-25 22:23:47 +0900

    carousel testing & img does not appear

--- activiting/src/ActivitingImg.js
+++ activiting/src/ActivitingImg.js
@@ -1,10 +1,24 @@
-
-import React, {Component} from "react";
-import ReactDOM from 'react-dom';
+import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 import "./ActivitingImg.css";
 
 export default function ActivitingImg() {
-  return <div />;
+  return (
+    <Carousel>
+    <div>
+      <img src='./src/img/main-body.jpg' alt="test1" />
+      <p className="legend">test</p>
+    </div>
+    <div>
+      <img src='../../src/img/main-top.jpg' alt="test2"/>
+      <p className="legend">test2</p>
+    </div>
+    <div>
+      <img src='../../src/img/test.png' alt="test3" />
+      <p className="legend">test3</p>
+    </div>
+  </Carousel>
+  )
 }
+
